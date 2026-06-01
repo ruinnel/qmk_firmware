fb30984ae5edd53f65924ef46f5a496c0fa0f0b5
ruinnel <ruinnel@gmail.com>
2026-06-01 23:44:25 +0900

    fix(iris_ce): fix RP2040 freeze and key repeats on Linux KVM switching

--- keyboards/keebio/iris_ce/keymaps/ruinnel/config.h
+++ keyboards/keebio/iris_ce/keymaps/ruinnel/config.h
@@ -22,3 +22,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_SPLIT
 #define RGBLIGHT_LED_COUNT 68
+
+// =======================================================
+// Dell 모니터의 KVM 스위치를 통해 연결 할 경우 Linux에서 키보드가 멈추는 현상 수정을 위한 옵션들
+// Linux의 usb driver가 전력 부하등의 처리에 민감해 전력 부하가 걸리는 LED를 끄고 추가로 아래 설정들이 필요.
+// VBUS 설정을 상위 핀(GP0) 그대로 유지하며 노이즈만 필터링합니다
+// =======================================================
+#undef USB_VBUS_PIN
+#define USB_VBUS_PIN GP0
+#define USB_VBUS_TIMEOUT 500  // KVM 전환 시 전압이 출렁여도 0.5초간 버티며 연결 유지
+
+// KVM 스위치 및 리눅스 환경을 위한 RP2040 방어 옵션
+#define HAL_USB_DRIVE_CURRENT_8MA     // RP2040 신호 강도 최대화
+#define USB_SUSPEND_WAKEUP_DELAY 1000  // 전환 시 1초 대기 안정화
+#define SPLIT_WATCHDOG_ENABLE         // Full-Duplex 통신 끊김 감시
+#define SPLIT_CONNECTION_CHECKER      // 통신 유실 시 강제 복구
--- keyboards/keebio/iris_ce/keymaps/ruinnel/rules.mk
+++ keyboards/keebio/iris_ce/keymaps/ruinnel/rules.mk
@@ -8,7 +8,7 @@ CONSOLE_ENABLE =  no        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
 ENCODER_ENABLE = no
@@ -17,3 +17,7 @@ OS_DETECTION_ENABLE = yes
 
 LTO_ENABLE = yes
 TRI_LAYER_ENABLE = yes
+
+# USB 통신 빈도를 낮춰 KVM 및 리눅스 버퍼 과부하 방지
+USB_POLLING_INTERVAL_MS = 8  # 125Hz로 설정 (기본값인 1ms/1000Hz에서 변경)
+SUSPEND_ENABLE = no  # 키보드가 절전 모드(Suspend)로 들어가는 기능을 아예 차단 [1]
